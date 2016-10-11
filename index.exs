db65b41f542f41af6590c5b6043130db4b930a8d
cognatus <diegojmartinezzm@gmail.com>
2016-10-11 11:06:20 -0500

    Para que demonios sirven los atomos?

--- index.exs
+++ index.exs
@@ -1,1 +1,23 @@
-IO.puts "QUe onda vato from Elixir"
+IO.puts "QUe onda vato\n from Elixir"
+
+add = fn a, b ->
+        a + b
+      end
+
+
+x = 82
+#Todo aqui general
+IO.puts add.(2,7)
+#Especificamente aqui, no afecta el entorno
+(fn ->
+  x = 12
+  IO.puts x
+end).()
+
+IO.puts x
+
+#Paara listas usar length
+length [2, 3, false, 'a', "asdasdas"]
+
+#Para tuplas usar tuple_size
+tuple_size {true, "a", :asda}
